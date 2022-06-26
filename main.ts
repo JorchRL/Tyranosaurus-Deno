7625b129b33d71abc07f39ca601fef97e4187e5b
Jorge <jorch.rl@gmail.com>
2022-06-25 21:28:38 -0500

    Add main.ts (ie. example app) for debugging

---
+++ main.ts
@@ -0,0 +1,11 @@
+// This script is an example app for debugging purposes only.
+
+import { App } from "./mod.ts";
+
+const TApp = new App();
+
+// TApp.run();
+
+// TApp.exit();
+
+console.log("Run to main.ts finish");
