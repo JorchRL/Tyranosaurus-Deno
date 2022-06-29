e791d3bcb55fd4357acd2507b68bacf8f4e4e619
Jorge <jorch.rl@gmail.com>
2022-06-28 23:28:46 -0500

    Add skeleton tests for the Application class

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -1,28 +1,21 @@
 import { describe, it } from "https://deno.land/std@0.144.0/testing/bdd.ts";
 
-describe("App:ProcessManager class implementation tests", () => {
-  describe("exit()", () => {
-    it("should exit (code:1) correctly when no subprocess is running", () => {
-    });
+// these are mostly integration tests (i think) for the core functionality of the library. All the main functionality involves a combination of the Application class and the client class (also the server). So I find it hard to think of unit tests for this.
 
-    it("should exit (code:0) correctly when one or more subprocess are running ", () => {
-    });
-
-    it("should exit (code:0) only after ending all running subprocesses", () => {
-    });
+describe("Tyrannosaurus.App class - core functionality", () => {
+  it("should create an app instance with a webview field initialized in the constructor", () => {
   });
-
-  describe("killSubprocess()", () => {
-    it("should kill (code: 0) an existing subprocess correctly.", () => {
-    });
-
-    it("should throw when kill subprocess failed (code:1)", () => {
-    });
-
-    it("should show a warning when killing a non existing subprocess (code:2)", () => {
-    });
+  it("should set the clientOptions", () => {
   });
-
-  describe("restartSubprocess()", () => {
+  it("should run and terminate the client correctly (via a bound callback)", () => {
+    // I am testing two things here, but there is not really any other way to automate this. As far as i know. Anyways, I still need to test the binding of callbacks, which is part of the core functionality. That is, the App class should never run a webview without at least the terminate() callback.
   });
+});
+
+describe("Tyrannosaurus.App - messaging with the client", () => {
+  it("should send a message to the client and get a response", () => {
+  });
+});
+
+describe("Tyrannosaurus.App - running the file server", () => {
 });
