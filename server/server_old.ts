3a487a15a006161a5935e2cc12e3b8c0efb5c434
Jorge <jorch.rl@gmail.com>
2022-06-28 22:16:35 -0500

    Deprecate old version of server.ts

---
+++ server/server_old.ts
@@ -0,0 +1,27 @@
+/**
+ * deprecated
+ */
+
+// import { contentType, getFile, randomId, serve } from "./deps.ts";
+
+// const handler = async (req: Request) => {
+//   const path = (new URL(req.url)).pathname;
+//   const stream = await getFile(path);
+
+//   if (!stream) return new Response(null, { status: 404 });
+
+//   // Get the extension se we can set the content-type header
+//   const ext = path.match(/\.[^\.]+$/);
+
+//   return new Response(stream, {
+//     status: 200,
+//     // @ts-ignore this will work just fine :D
+//     headers: {
+//       "x-respose-identifier": await randomId(),
+//       "Content-Type": ext ? contentType(ext[0]) : contentType(".html"),
+//     },
+//   });
+// };
+
+// console.log("Server started!");
+// serve(handler, { port: 5500 });
